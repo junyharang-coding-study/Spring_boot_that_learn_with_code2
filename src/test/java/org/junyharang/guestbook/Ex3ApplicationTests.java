a2fda2a811b007aeb38419dfe1ecedc1f67cabd3
junyharang <junyharang8592@gmail.com>
2021-10-27 19:28:28 +0900

    211027 : 실습을 위한 초기 구성 완료

---
+++ src/test/java/org/junyharang/guestbook/Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.junyharang.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
